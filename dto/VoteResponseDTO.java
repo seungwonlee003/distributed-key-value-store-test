f1064d2505c6f8d3a9cfeb72a64f40a025d06348
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-22 18:58:59 +0900

    Update VoteResponseDTO.java

--- dto/VoteResponseDTO.java
+++ dto/VoteResponseDTO.java
@@ -1,1 +1,45 @@
+package com.example.raft.dto;
 
+/**
+ * Data Transfer Object for the RequestVote RPC response in Raft.
+ * Sent by nodes to the candidate indicating whether the vote was granted.
+ */
+public class VoteResponseDTO {
+    private int term;         // Current term, for candidate to update itself
+    private boolean voteGranted; // True if the candidate received the vote
+
+    // Default constructor for serialization
+    public VoteResponseDTO() {
+    }
+
+    // Constructor with all fields
+    public VoteResponseDTO(int term, boolean voteGranted) {
+        this.term = term;
+        this.voteGranted = voteGranted;
+    }
+
+    // Getters and Setters
+    public int getTerm() {
+        return term;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public boolean isVoteGranted() {
+        return voteGranted;
+    }
+
+    public void setVoteGranted(boolean voteGranted) {
+        this.voteGranted = voteGranted;
+    }
+
+    @Override
+    public String toString() {
+        return "VoteResponseDTO{" +
+                "term=" + term +
+                ", voteGranted=" + voteGranted +
+                '}';
+    }
+}
