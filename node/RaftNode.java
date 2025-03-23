e38946cf8369a83e0593f3f91b011318bd6a88ff
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-23 13:36:11 +0900

    Update RaftNode.java

--- node/RaftNode.java
+++ node/RaftNode.java
@@ -24,23 +24,6 @@ public class RaftNode {
         this.heartbeatManager = new HeartbeatManager(this);
     }
 
-    @PostConstruct
-    public void start() {
-        if (state.getRole() == Role.FOLLOWER || state.getRole() == Role.CANDIDATE) {
-            electionManager.resetElectionTimer();
-        } else if (state.getRole() == Role.LEADER) {
-            becomeLeader();
-        }
-    }
-
-    public synchronized VoteResponseDTO handleVoteRequest(RequestVoteDTO requestVote) {
-        return electionManager.handleVoteRequest(requestVote);
-    }
-
-    public void resetElectionTimer() {
-        electionManager.resetElectionTimer();
-    }
-
     private void becomeLeader() {
         state.setRole(Role.LEADER);
         System.out.println("Node " + state.getNodeId() + " became leader for term " + state.getCurrentTerm());
