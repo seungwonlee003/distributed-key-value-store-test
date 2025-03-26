3cebf810edaaaebc1cd87c6ef37cf1d9e61c13f8
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-26 13:31:18 +0900

    Update ConsistencyLevel.java

--- ConsistencyLevel.java
+++ ConsistencyLevel.java
@@ -1,8 +1,15 @@
-package com.example.raft.model;
-
 public enum ConsistencyLevel {
     DEFAULT,
     READ_YOUR_WRITES,
     LEADER_LOCAL,
     EVENTUAL;
+
+    public static ConsistencyLevel fromString(String value) {
+        try {
+            return valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid consistency level: " + value + 
+                ". Valid options: STRONG, LEADER_LOCAL, EVENTUAL");
+        }
+    }
 }
