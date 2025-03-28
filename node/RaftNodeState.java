a54e2756c02372405dcde9664143ebc8d8e2b39a
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-28 20:17:46 +0900

    Update RaftNodeState.java

--- node/RaftNodeState.java
+++ node/RaftNodeState.java
@@ -1,66 +1,21 @@
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+// Persistent state on all servers:
+@Getter
+@Setter
 public class RaftNodeState {
-    // Persistent state on all servers:
     private int currentTerm = 0;  
     private Integer votedFor = null; 
     
     private final int nodeId;       
-    // start with a role of follower
     private Role currentRole = Role.FOLLOWER;
     private Integer currentLeader = null;  
 
-    private int lastApplied = 0; // Add this field
-
-    public int getLastApplied() {
-        return lastApplied;
-    }
-
-    public void setLastApplied(int lastApplied) {
-        this.lastApplied = lastApplied;
-    }
+    private int lastApplied = 0; 
 
     // get nodeId from the application.properties
     public RaftNodeState(int nodeId) {
         this.nodeId = nodeId;
-    }
-
-    // ---------------- Getters and Setters ---------------- //
-
-    public int getCurrentTerm() {
-        return currentTerm;
-    }
-
-    public void setCurrentTerm(int currentTerm) {
-        this.currentTerm = currentTerm;
-    }
-
-    public Integer getVotedFor() {
-        return votedFor;
-    }
-
-    public void setVotedFor(Integer votedFor) {
-        this.votedFor = votedFor;
-    }
-
-    public int getNodeId() {
-        return nodeId;
-    }
-
-    public Role getCurrentRole() {
-        return currentRole;
-    }
-
-    public void setCurrentRole(Role currentRole) {
-        this.currentRole = currentRole;
-    }
-
-    public Integer getCurrentLeader() {
-        return currentLeader;
-    }
-
-    public void setCurrentLeader(Integer currentLeader) {
-        this.currentLeader = currentLeader;
     }
 }
