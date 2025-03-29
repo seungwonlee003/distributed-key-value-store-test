98315c2b5b74e7b20583b0071c8e253820436c15
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 15:20:56 +0900

    Update RaftInitializer.java

--- config/RaftInitializer.java
+++ config/RaftInitializer.java
@@ -3,26 +3,20 @@
 public class RaftInitializer {
 
     private final RaftNodeState nodeState;
+    private final RaftStateManager stateManager;
     private final RaftNodeConfig config;
 
     @PostConstruct
     public void init() {
         nodeState.setNodeId(config.getNodeId());
-        nodeState.setRole(Role.FOLLOWER);
-        nodeState.setCurrentTerm(0);
-        nodeState.setVotedFor(null);
-
-        System.out.println("Node " + nodeState.getNodeId() + " starting as FOLLOWER");
 
         if (config.getPeerUrls() != null && !config.getPeerUrls().isEmpty()) {
             nodeState.setPeerUrls(config.getPeerUrls());
-            System.out.println("Peer URLs set from config: " + config.getPeerUrls());
         } else {
             throw new IllegalStateException("Peer URLs must be defined in configuration.");
         }
 
-        nodeState.resetElectionTimer();
-        System.out.println("Election timer started.");
+        stateManager.becomeFollower(0);
     }
 }
 
