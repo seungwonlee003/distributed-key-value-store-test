71232a52f656b0e86a8dd62ad353b858c36ffefa
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 13:43:55 +0900

    Update HeartbeatManager.java

--- log/HeartbeatManager.java
+++ log/HeartbeatManager.java
@@ -2,6 +2,7 @@ import java.util.concurrent.*;
 
 public class HeartbeatManager {
     private final RaftLogManager raftLogManager;
+    private final RaftNodeState raftNodeState;
     private final ElectionManager electionManager;
     ScheduledExecutorService heartbeatExecutor = Executors.newSingleThreadScheduledExecutor();
     private ScheduledFuture<?> heartbeatFuture;
@@ -11,7 +12,7 @@ public class HeartbeatManager {
         stopHeartbeats();
         electionManager.cancelElectionTimerIfRunning();
         heartbeatFuture = heartbeatExecutor.scheduleAtFixedRate(() -> {
-            if (raftNode.getRole() == Role.LEADER) {
+            if (raftNodeState.getRole() == Role.LEADER) {
                 raftLogManager.startLogReplication();
             }
         }, 0, 1000, TimeUnit.MILLISECONDS);
