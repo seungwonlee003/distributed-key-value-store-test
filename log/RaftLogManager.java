92ee257b8da06e6e5bd7d6c0676836949114f19e
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 14:08:15 +0900

    Update RaftLogManager.java

--- log/RaftLogManager.java
+++ log/RaftLogManager.java
@@ -4,23 +4,11 @@
  */
 
 @Service
+@RequiredArgsConstructor
 public class RaftLogManager {
     private final ClientRequestHandler clientRequestHandler;
     private final LogReplicator logReplicator;
     private final AppendEntriesHandler appendEntriesHandler;
-
-    public RaftLogManager(
-        RaftConfig config,
-        RaftLog log,
-        RaftStateManager stateManager,
-        RaftNodeState nodeState,
-        RestTemplate restTemplate,
-        StateMachine stateMachine
-    ) {
-        this.clientRequestHandler = new ClientRequestHandler(log, nodeState, config);
-        this.logReplicator = new LogReplicator(config, log, stateManager, nodeState, restTemplate, stateMachine);
-        this.appendEntriesHandler = new AppendEntriesHandler(log, stateManager, nodeState, stateMachine);
-    }
 
     public boolean handleClientRequest(LogEntry entry) {
         return clientRequestHandler.handle(entry);
