ec4f29c264e742a254f909e051d4e158dabf8b52
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 13:38:08 +0900

    Update ClientRequestHandler.java

--- log/ClientRequestHandler.java
+++ log/ClientRequestHandler.java
@@ -12,17 +12,27 @@ public class ClientRequestHandler {
     public boolean handle(LogEntry clientEntry) {
         raftLog.append(clientEntry);
         int entryIndex = raftLog.getLastIndex();
+    
         long start = System.currentTimeMillis();
-        long timeoutMillis = raftConfig.getClientRequestTimeoutMillis();
+        long timeoutMillis = raftConfig.getClientRequestTimeoutMillis(); 
 
+        // wait for at most 5 seconds for the client's write to be acknowledged by the majority
         while (raftNodeState.getRole() == Role.LEADER) {
-            if (raftLog.getCommitIndex() >= entryIndex) return true;
-            if (System.currentTimeMillis() - start > timeoutMillis) return false;
-            try { Thread.sleep(1000); } catch (InterruptedException e) {
+            if (raftLog.getCommitIndex() >= entryIndex) {
+                return true;
+            }
+            if (System.currentTimeMillis() - start > timeoutMillis) {
+                return false;
+            }
+    
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 return false;
             }
         }
-        return false;
+    
+        return false; 
     }
 }
