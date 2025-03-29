3db5e38f36a0ad474386bb772d1582e8063053b8
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 13:40:59 +0900

    Update AppendEntriesHandler.java

--- log/AppendEntriesHandler.java
+++ log/AppendEntriesHandler.java
@@ -1,1 +1,63 @@
+public class AppendEntriesHandler {
+    private final RaftLog log;
+    private final RaftStateManager stateManager;
+    private final RaftNodeState nodeState;
+    private final StateMachine stateMachine;
 
+    public AppendEntriesHandler(RaftLog log, RaftStateManager sm, RaftNodeState ns, StateMachine smachine) {
+        this.log = log;
+        this.stateManager = sm;
+        this.nodeState = ns;
+        this.stateMachine = smachine;
+    }
+
+    public synchronized AppendEntryResponseDTO handle(AppendEntryDTO dto) {
+        int term = nodeState.getCurrentTerm();
+        int leaderTerm = dto.getTerm();
+
+        if (leaderTerm < term) return new AppendEntryResponseDTO(term, false);
+        if (leaderTerm > term) {
+            stateManager.becomeFollower(leaderTerm);
+            term = leaderTerm;
+        }
+
+        if (dto.getPrevLogIndex() > 0 &&
+            (!log.containsEntryAt(dto.getPrevLogIndex()) ||
+             log.getTermAt(dto.getPrevLogIndex()) != dto.getPrevLogTerm())) {
+            return new AppendEntryResponseDTO(term, false);
+        }
+
+        int index = dto.getPrevLogIndex() + 1;
+        List<LogEntry> entries = dto.getEntries();
+        if (!entries.isEmpty()) {
+            if (log.containsEntryAt(index) && log.getTermAt(index) != entries.get(0).getTerm()) {
+                log.deleteFrom(index);
+            }
+            log.appendAll(entries);
+        }
+
+        if (dto.getLeaderCommit() > log.getCommitIndex()) {
+            int lastNew = dto.getPrevLogIndex() + entries.size();
+            log.setCommitIndex(Math.min(dto.getLeaderCommit(), lastNew));
+            applyCommittedEntries();
+        }
+
+        stateManager.resetElectionTimer();
+        return new AppendEntryResponseDTO(term, true);
+    }
+
+    private void applyCommittedEntries() {
+        int commitIndex = log.getCommitIndex();
+        int lastApplied = nodeState.getLastApplied();
+        for (int i = lastApplied + 1; i <= commitIndex; i++) {
+            try {
+                LogEntry entry = log.getEntryAt(i);
+                stateMachine.apply(entry);
+                nodeState.setLastApplied(i);
+            } catch (Exception e) {
+                System.out.println("Apply failed at index " + i);
+                System.exit(1);
+            }
+        }
+    }
+}
