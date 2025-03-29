520863583c6a8ca8f1b056b12f187f23fe661426
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 14:51:14 +0900

    Update RaftNodeState.java

--- node/RaftNodeState.java
+++ node/RaftNodeState.java
@@ -1,26 +1,14 @@
-import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-// Persistent state on all servers:
 @Getter
 @Setter
 @Component
 @RequiredArgsConstructor
-@NoArgsConstructor
 public class RaftNodeState {
-    private int currentTerm = 0;  
-    private Integer votedFor = null; 
-    
-    private final int nodeId;       
+    private final int nodeId;
+
+    private int currentTerm = 0;
+    private Integer votedFor = null;
     private Role currentRole = Role.FOLLOWER;
-    private Integer currentLeader = null;  
-
-    private int lastApplied = 0; 
-
-    private List<Integer> peerUrls;
-
-    // get nodeId from the application.properties
-    public RaftNodeState(int nodeId) {
-        this.nodeId = nodeId;
-    }
+    private Integer currentLeader = null;
+    private int lastApplied = 0;
+    private List<String> peerUrls; // Assuming URLs
 }
