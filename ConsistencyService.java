d8ab74435a1860fc4e6b1c351db4a32047003d7e
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-29 14:24:53 +0900

    Update ConsistencyService.java

--- ConsistencyService.java
+++ ConsistencyService.java
@@ -5,16 +5,12 @@ import com.example.raft.storage.KVStore;
 import com.example.raft.RaftNode;
 import com.example.raft.RaftLogManager;
 
+@Service
+@RequiredArgsConstructor
 public class ConsistencyService {
     private final RaftNode raftNode;
     private final RaftLogManager raftLogManager;
     private final KVStore kvStore;
-
-    public ConsistencyService(RaftNode raftNode, RaftLogManager raftLogManager, KVStore kvStore) {
-        this.raftNode = raftNode;
-        this.raftLogManager = raftLogManager;
-        this.kvStore = kvStore;
-    }
 
     public String read(String key, ConsistencyLevel level) throws ConsistencyException {
         switch (level) {
