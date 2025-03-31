a9c7263b1132b2c11e84e24501fee967ab842a84
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-03-31 10:17:51 +0900

    Update RaftNodeState.java

--- node/RaftNodeState.java
+++ node/RaftNodeState.java
@@ -3,10 +3,13 @@
 @Component
 @RequiredArgsConstructor
 public class RaftNodeState {
+    // non-volatile
     private final int nodeId;
-
     private int currentTerm = 0;
     private Integer votedFor = null;
+
+    // volatile
     private Role currentRole = Role.FOLLOWER;
+    private int currentLeader;
     private int lastApplied = 0;
 }
