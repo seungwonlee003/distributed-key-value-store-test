eab94c4651a4442556e4290bad116341602157df
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-04 16:24:27 +0900

    Update ConfirmLeaderShipRequestDTO.java

--- dto/ConfirmLeaderShipRequestDTO.java
+++ dto/ConfirmLeaderShipRequestDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class ConfirmLeaderShipRequestDTO {
+public class ConfirmLeadershipRequestDTO {
     private String nodeId;
     private int term;
 }
