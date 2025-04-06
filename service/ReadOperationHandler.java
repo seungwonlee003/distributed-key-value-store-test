42b682b8aaae49bacc28febe50cb26cf723abf03
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-06 13:21:35 +0900

    Update ReadOperationHandler.java

--- service/ReadOperationHandler.java
+++ service/ReadOperationHandler.java
@@ -9,40 +9,10 @@ public class ReadOperationHandler {
     private final LeadershipManager leadershipManager;
 
     public String handleRead(String key) throws IllegalStateException {
-        if (raftConfig.isEnableFollowerReads()) {
-            if (!raftNodeState.isLeader()) {
-                int readIndex = requestReadIndexFromLeader();
-                waitForLogToSync(readIndex);
-                return kvStore.get(key);
-            } else {
-                return performLeaderRead(key);
-            }
-        } else {
-            if (!raftNodeState.isLeader()) {
-                String leaderUrl = raftNodeState.getCurrentLeaderUrl();
-                throw new IllegalStateException("Read requests must be routed to the leader at " + leaderUrl + " when follower reads are disabled");
-            }
-            return performLeaderRead(key);
-        }
-    }
-
-    private int requestReadIndexFromLeader() {
-        String leaderUrl = raftNodeState.getCurrentLeaderUrl();
-        if (leaderUrl == null) {
-            throw new IllegalStateException("Leader unknown. Cannot perform follower read.");
-        }
-        try {
-            ResponseEntity<ReadIndexResponseDTO> response = restTemplate.getForEntity(
-                leaderUrl + "/raft/rpc/readIndex",
-                ReadIndexResponseDTO.class
-            );
-            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-                return response.getBody().getReadIndex();
-            }
-            throw new IllegalStateException("Failed to obtain read index from leader. Status: " + response.getStatusCode());
-        } catch (Exception e) {
-            throw new IllegalStateException("Error while contacting leader for read index", e);
-        }
+        leadershipManager.confirmLeadership();
+        int readIndex = raftLogManager.getCommitIndex();
+        waitForLogToSync(readIndex);
+        return kvStore.get(key);
     }
 
     private void waitForLogToSync(int readIndex) {
@@ -54,12 +24,5 @@ public class ReadOperationHandler {
                 throw new IllegalStateException("Interrupted while syncing log for read", e);
             }
         }
-    }
-
-    private String performLeaderRead(String key) {
-        leadershipManager.confirmLeadership();
-        int readIndex = raftLogManager.getCommitIndex();
-        waitForLogToSync(readIndex);
-        return kvStore.get(key);
     }
 }
