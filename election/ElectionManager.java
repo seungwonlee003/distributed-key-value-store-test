685eb8a86d374461831c45b7bcab32560ea362c8
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-06 18:30:08 +0900

    Update ElectionManager.java

--- election/ElectionManager.java
+++ election/ElectionManager.java
@@ -48,7 +48,7 @@ public class ElectionManager {
         return new VoteResponseDTO(currentTerm, true);
     }
 
-    //  election timer duration must be greater than election rpc timeout or else concurrency problems may arise
+    //  election timer duration must be strictly greater than election rpc timeout or else concurrency problems may arise
     public void startElection() {
         synchronized (this) {
             if (nodeState.getRole() == Role.LEADER) return;
